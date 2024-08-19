cbfdd4aea9eacb3a1a62346bd808b4179a13c3d9
unknown <01234aag@gmail.com>
2024-08-19 16:24:51 +0530

    Added new prompts

---
+++ MyInfo.py
@@ -0,0 +1,176 @@
+resume_points="""
+**Ashish Sharma**  
+Graduated from - Indian Institute of Technology Kanpur , in Physics   
+Phone: +91-9116775937 | Email: 01234aag@gmail.com 
+GitHub: https://github.com/ashishSharma222003
+LinkedIn: https://www.linkedin.com/in/ashish-sharma-218498248/
+
+
+### **Education:**
+
+- **Bachelor of Science (B.Sc.) in Physics**  
+  *Indian Institute of Technology, Kanpur | 2020 - 2024*  
+  - Achieved a Cumulative Performance Index (CPI) of 6.0/10.
+  - Developed a strong foundation in physics, coupled with exposure to advanced topics in computational physics, electronics, and data analysis.
+  - Engaged in various interdisciplinary projects, applying physical principles to real-world problems.
+
+- **Class XII (CBSE Board)**  
+  *Oxford International Academy, Jaipur | 2019 - 2020*  
+  - Secured 89.6% in the Central Board of Secondary Education (CBSE) exams.
+  - Excelled in physics , chemistry and mathematics, demonstrating analytical and problem-solving skills.
+  - Actively participated in science fairs and competitions, fostering a passion for research and innovation.
+
+- **Class X (CBSE Board)**  
+  *Defence Public School, Jaipur | 2018*  
+  - Scored 91.6% in the CBSE board examinations.
+  - Recognized for academic excellence and consistent performance across all subjects.
+  - Participated in extracurricular activities, including sports and science clubs, showcasing a well-rounded academic profile.
+
+---
+
+### **Work Experience:**
+
+- **Gen AI Engineer | Valiance Solutions**  
+  *February 2024 - August 2024*  
+  - Spearheaded the development of a generative AI-based system aimed at extracting specific information from complex documents.
+  - Implemented Google Chat Bison, Gemini, llama3 and chatgpt on Google Cloud Platform (GCP) as part of a Retrieval-Augmented Generation (RAG) framework to enhance information retrieval.
+  - Employed advanced frameworks like Langchain and Llamaindex to structure and optimize the RAG process, ensuring high accuracy in the retrieval of relevant data.
+  - Designed and utilized a Reranker and Sentence Window Retriever to identify and retrieve the most pertinent information from large datasets.
+  - Evaluated the system's performance using RAGAS metrics and conducted a comparative analysis with human evaluations to refine the AI's accuracy and reliability.
+  - Developed a Text-to-SQL project, leveraging Large Language Models (LLMs) like Gemini-Pro, to automatically generate SQL queries, streamlining database management and querying processes.
+  - Created a document classification model using Google’s Document AI, automating the categorization of documents into their respective classes, thereby improving organizational efficiency.
+  - Developed a Flask application for the backend and utilized Firestore (a NoSQL database) for storing chat data.
+  - Deployed Llama3 on Google Cloud Compute Engine for hosting.
+  - Implemented parallelism to accelerate the embedding process.
+  - Utilized the BeautifulSoup web scraping library to extract data related to the Kumbh Mela.
+  - Developed an agentic application that organizes and summarizes emails along with their attachments, phone call recordings, and messages.
+
+
+- **AI Developer | Hrrkoin**  
+  *August 2023 - January 2024*  
+  - Led the enhancement and restructuring of HR management processes with a focus on increasing accountability, safety, and efficiency through AI-driven solutions.
+  - Integrated advanced Natural Language Processing (NLP) techniques to enable the AI system to accurately interpret and respond to candidate responses during interviews.
+  - Proposed and implemented an intricate algorithmic framework that dynamically adjusts interview questions based on real-time candidate responses, allowing for a more tailored and comprehensive assessment process.
+  - Integrated emotion recognition capabilities into the AI system, enabling the detection of subtle emotional cues during conversations, thereby providing deeper insights into the candidate’s suitability for the role.
+  - Seamlessly integrated GPT-3 into the Hrrkoin website, enhancing the user experience by enabling dynamic, context-aware interactions with visitors seeking HR-related information and services.
+
+- **ML Developer | Xatalyst Labs**  
+  *October 2023 - March 2024*  
+  - Played a key role in revolutionizing personal fitness by developing a personalized AI that utilizes deep learning models to provide real-time feedback on posture during exercises.
+  - Used OpenCV to capture and analyze posture, extracting key movement patterns that are fed into a deep learning model for analysis.
+  - Designed and developed an efficient and lightweight deep learning model using state-of-the-art architectures like MobileNet, ensuring compatibility and seamless operation on mobile devices.
+  - The AI system provided users with instant, personalized feedback on their posture, helping to prevent injuries, improve exercise performance, and enhance overall fitness outcomes.
+
+---
+
+### **Key Projects:**
+
+- **Semantic Segmentation Project | Self-Directed**  
+  - Undertook a self-directed project to explore the application of semantic segmentation in self-driving cars.
+  - Leveraged the U-net architecture, augmented with ResNet blocks, to accurately segment images into different classes, such as buildings, vehicles, pedestrians, and roads.
+  - Trained the model using annotated images, focusing on improving the system's ability to distinguish between different elements in a driving scene, with potential applications in autonomous vehicle technology.
+  - Employed both convolutional and deconvolutional layers to capture local and global image features, enhancing the model’s precision in complex environments.
+
+- **Image Verification and Recognition Project | Self-Directed**  
+  - Developed a neural network architecture (VGG-16) for image verification and recognition using one-shot learning, achieving high accuracy on test datasets.
+  - Implemented Siamese networks and fine-tuned them to learn similarity metrics between images, which significantly improved the model’s accuracy and robustness.
+  - Optimized triplet loss functions to enhance the model's performance in recognizing and categorizing new images, making it a powerful tool for various image recognition tasks.
+
+- **Car Detection with YOLO Project | Self-Directed**  
+  - Utilized the YOLOv2 model for object detection, focusing on detecting cars in complex environments.
+  - Leveraged a pre-trained model to handle the substantial computational requirements, enabling efficient and accurate automobile detection.
+  - Improved the overall accuracy of the model by applying the Non-Maximum Suppression (NMS) algorithm and adjusting the Intersection-over-Union (IoU) parameter to 0.6, reducing the occurrence of overlapping anchor boxes.
+
+- **Trigger Word Detection Project | Self-Directed**  
+  - Created an audio dataset using synthesized data and developed an algorithm for detecting wake words, a key component in voice-activated systems.
+  - Employed audio spectrograms as a preprocessing step, which were then fed into a 1-D convolutional layer before being processed by Recurrent Neural Network (RNN) layers, including Gated Recurrent Units (GRU) and Long Short-Term Memory (LSTM) layers, to accurately detect trigger words in real-time.
+
+---
+
+### **Technical Skills:**
+
+- **Software and Utilities:**  
+  - Proficient in Git for version control, LATEX for document preparation, Jupyter for interactive computing, Excel for data analysis, Linux for operating system management,  Google Cloud Platform (GCP) for cloud services like Vertex AI, SQL, Firestore, Buckets, VM.
+  
+- **Programming Languages and Libraries:**  
+  - Experienced in SQL for database management, C++ for system programming, Python for scripting and automation, and OpenCV for computer vision tasks.
+  - Skilled in using advanced AI and ML frameworks such as LlamaIndex, Langchain, Flask, TensorFlow, Keras, and FastAPI for developing and deploying machine learning models and web applications.
+  - Utilized Streamlit to develop visually compelling dashboards for RAG applications.
+
+---
+
+### **Relevant Courses:**
+
+- **Core Courses:**
+  - Introduction to Programming, Probability and Statistics, Computational Physics, Data Structure and Algorithm, Introduction to Electronics, Image Processing, and Introduction to Machine Learning.
+
+- **Specialized Online Courses:**
+  - Machine Learning Specialization, Deep Learning Specialization, MLOps Specialization.
+
+---
+
+### **Positions of Responsibility/Volunteer:**
+
+- **Secretary, Rithambara | Antragani, IIT Kanpur**  
+  *April 2020 - March 2021*  
+  - Organized and managed Rithambara events, establishing connections with various organizations and individuals through LinkedIn and other social media platforms.
+  - Played a key role in developing sponsorship proposals, securing funding, and ensuring the successful execution of events.
+  - Reviewed and cross-checked participant data from previous events, updating and expanding the list of participating entities to ensure accurate and comprehensive records.
+
+---
+
+### **Other Interests:**
+
+- **Internet of Things (IoT) and Quantum Computing:**
+  - Enthusiastic about the practical applications of IoT and Quantum Computing in various fields, exploring how these technologies can revolutionize industries.
+  
+- **Badminton:**
+  - Passionate about playing badminton, regularly participating in matches to maintain physical fitness and competitive spirit.
+
+
+
+This comprehensive view of Ashish Sharma’s skills, experiences, and achievements, highlighting his technical expertise and leadership roles.
+"""
+agent_prompt1="""
+- You are Ashish Sharma's personal assistant, tasked with assisting recruiters and others who wish to connect with him.
+- Always answers in bullets that are comprehensive.
+- Provide information about Ashsih Sharma on Ashish Sharma behalf and emphasize Ashish Sharma strong work ethic.
+- Collect information from individuals who want to contact Ashish Sharma.
+- Respond in a natural, human-like manner to ensure your answers are indistinguishable from those given by a real Human being.
+- You have only 2 iteration to respond.
+**IMPORTANT**
+1.UTILIZE all the information to give answer in very comprehensive way.
+2.Answer in Markdown format.
+"""
+agent_prompt=(
+        "You are Ashish Sharma's personal assistant, tasked with assisting recruiters and others who wish to connect with Ashish.\n"
+        "Instruction -\n"
+        "1. Always understand what the question is asking.\n"
+        "2. Break down the question into multiple parts. Answer each part separately, and then combine the answers to form a comprehensive response.\n"
+        "3. Utilize the information provided to formulate a response to the query concerning Ashish Sharma.\n"
+        "4. Always answers in bullets that are comprehensive.\n\n"
+        "**IMPORTANT**\n"
+        "Do not alter or rephrase the information received from the tool; provide the response exactly as information received.\n"
+        "If the query is not related to Ashish Sharma, respond by stating that you can only provide information about Ashish Sharma.\n"
+        "Always respond in Bullets"
+)
+
+
+firstResponse = """
+Feel free to ask for any information about Ashish Sharma.
+
+You can also share your contact details, email address, or link on this app, and it will be forwarded to me.
+
+**Some Example Queries:**
+- My name is John Doe and my email id is xyz@gmail.com/phone number/linkedin. I want to contact Ashish Sharma.
+- Tell me about his academics.
+- What is his experience like?
+"""
+
+    
+
+
+
+
+if __name__=="__main__":
+    print(resume_points)
